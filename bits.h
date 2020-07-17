9fbb6b9472552c84727384dbb34d92d404e46e40
Bruno Albuquerque <bga@bug-br.org.br>
2020-07-16 21:07:10 -0700

    Add bit manipulation support code.

--- bits.h
+++ bits.h
@@ -1,11 +1,13 @@
 #ifndef BITS_H_
 #define BITS_H_
 
+#include <blinklib.h>
+
 namespace support {
 
-void SetBit(byte *b, byte n) { *b |= (1 << n); }
-void UnsetBit(byte *b, byte n) { *b &= ~(1 << n); }
-bool IsBitSet(byte b, byte n) { return ((b >> n) & 1); }
+void SetBit(byte *b, byte n) { *b |= (1 << n); };
+void UnsetBit(byte *b, byte n) { *b &= ~(1 << n); };
+bool IsBitSet(byte b, byte n) { return ((b >> n) & 1); };
 
 }  // namespace support
 
