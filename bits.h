216f24054238490a40015b3a12c6d37de97c29a1
Bruno Albuquerque <bga@bug-br.org.br>
2020-07-26 14:25:40 -0700

    Separate definition and declartetion of bits stuff.

--- bits.h
+++ bits.h
@@ -5,9 +5,9 @@
 
 namespace support {
 
-void SetBit(byte *b, byte n) { *b |= (1 << n); };
-void UnsetBit(byte *b, byte n) { *b &= ~(1 << n); };
-bool IsBitSet(byte b, byte n) { return ((b >> n) & 1); };
+void SetBit(byte *b, byte n);
+void UnsetBit(byte *b, byte n);
+bool IsBitSet(byte b, byte n)};
 
 }  // namespace support
 
